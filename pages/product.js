e1fd23101d0038e583d60f106423078096a42d40
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2022-08-19 20:29:18 -0400

    Update product.js

--- pages/product.js
+++ pages/product.js
@@ -1,9 +1,12 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 
 const Product = () => {
     return (
-        <div className="space-x-4 p-6 bg-blue-400 border rounded-lg shadow-sm text-white flex items-center max-w-sm mx-auto">
-            <div className="text-xl font-medium">ChitChat</div>
-        </div>
+                                    
+                              
+<button className="btn btn-info">Info</button>
+                            
+                        
     )
 }
 
